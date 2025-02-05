1165138928fba9f2efd8f6786402619883b89020
Annaliese Watson <watsannas@gmail.com>
2025-02-04 22:50:20 -0500

    v1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
+
+  base: "/aswcreatives/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
